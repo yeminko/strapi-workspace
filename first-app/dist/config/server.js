363d228904629ef28ec11c940dad8ddab603b33e
Ye Min Ko <yeminko47@gmail.com>
2022-10-19 16:53:45 +0630

    [added] ckeditor

--- first-app/dist/config/server.js
+++ first-app/dist/config/server.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+    url: env("PUBLIC_URL", "http://localhost:1337"),
     app: {
-        keys: env.array('APP_KEYS'),
+        keys: env.array("APP_KEYS"),
     },
 });
